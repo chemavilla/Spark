04a1157c9986815496d3d787de8e501d5e5cf650
Jose Maria Villafañe Iravedra <85357729+chemavilla@users.noreply.github.com>
2024-01-03 17:44:52 +0100

    add ads table and join with entry price

--- car_schema.py
+++ car_schema.py
@@ -6,7 +6,7 @@ schema_price=StructType([
     StructField("Genmodel"   , StringType() , False),
     StructField("Genmodel_ID", StringType() , False),
     StructField("Year"       , IntegerType(), False),
-    StructField("Price"      , IntegerType(), False)])
+    StructField("Entry_Price", IntegerType(), False)])
  
 schema_sales=StructType([
     StructField("Maker"      , StringType() , False),
@@ -16,3 +16,32 @@ schema_sales=StructType([
 #Sales table has 20 integer fields [2000...2020]
 for i in range(0,20):
     schema_sales.add(StructField(f"20{i:02d}", IntegerType(), False))
+
+schema_trim=StructType([
+    StructField("Genmodel_ID", StringType() , False),
+    StructField("Maker"      , StringType() , False),
+    StructField("Genmodel"   , StringType() , False),
+    StructField("Trim"       , StringType() , False),
+    StructField("Year"       , IntegerType(), False),
+    StructField("Price"      , IntegerType(), False),
+    StructField("Emission"   , IntegerType(), False),
+    StructField("Fuel"       , StringType() , False),
+    StructField("Engine"     , IntegerType(), False)])
+
+schema_ads=StructType([
+    StructField("Adv_Maker"       , StringType() , False),
+    StructField("Adv_Genmodel"    , StringType() , False),
+    StructField("Adv_Genmodel_ID" , StringType() , False),
+    StructField("Adv_ID"      , StringType() , False),
+    StructField("Adv_year"    , StringType() , False),
+    StructField("Adv_month"   , IntegerType(), False),
+    StructField("Color"       , StringType() , False),
+    StructField("Reg_year"    , StringType() , False),
+    StructField("Bodytype"    , StringType() , False),
+    StructField("Runned_Miles", IntegerType(), False),
+    StructField("Engin_size"  , StringType() , False),
+    StructField("Gearbox"     , StringType() , False),
+    StructField("Fuel_type"   , StringType() , False),
+    StructField("Price"       , IntegerType(), False),
+    StructField("Seat_num"    , IntegerType(), False),
+    StructField("Door_num"    , IntegerType(), False)])
--- wrangling.py
+++ wrangling.py
@@ -6,14 +6,32 @@ spark = SparkSession.builder.master("local[1]") \
                     .getOrCreate()
 
 base_path= 'car_tables/'
-dfp=spark.read.csv(base_path+'Price_table.csv',header=False, schema=schema_price)
-dfs=spark.read.csv(base_path+'Sales_table.csv',header=False, schema=schema_sales)
+df_price=spark.read.csv(base_path+'Price_table.csv',header=True, schema=schema_price)
+df_sales=spark.read.csv(base_path+'Sales_table.csv',header=True, schema=schema_sales)
+df_trim =spark.read.csv(base_path+'Trim_table.csv' ,header=True, schema=schema_trim)
+df_ads  =spark.read.csv(base_path+'Ad_table.csv'   ,header=True, schema=schema_ads)
+
 
 #remove outliers from price table, 3 * stddev
-stddevp  = dfp.agg({'Price': 'stddev'}).collect()[0][0]
-meanp    = dfp.agg({'Price': 'mean'}).collect()[0][0]
+stddevp  = df_price.agg({'Entry_Price': 'stddev'}).collect()[0][0]
+meanp    = df_price.agg({'Entry_Price': 'mean'}).collect()[0][0]
 out_highp= meanp + (3*stddevp)
 out_lowp = meanp - (3*stddevp)
 
-dfp=dfp.where(f"Price > {out_lowp} AND Price < {out_highp}")
-dfp.show()
+df=df_price.where(f"Entry_Price > {out_lowp} AND Entry_Price < {out_highp}")
+
+#comprobar duplicados y valores null
+
+#quitar columnas
+
+
+#Adding entry price from new to Ads Table for reference
+join_cond_ad=(df_price.Maker       == df_ads.Adv_Maker      ) & \
+             (df_price.Genmodel    == df_ads.Adv_Genmodel   ) & \
+             (df_price.Genmodel_ID == df_ads.Adv_Genmodel_ID) & \
+             (df_price.Year        == df_ads.Adv_year       ) 
+
+df_ads = df_ads.join(df_price, on=join_cond_ad, how='left')
+
+dupli_cols=['Maker', 'Genmodel', 'Genmodel_ID']
+df_ads = df_ads.drop(*dupli_cols)
